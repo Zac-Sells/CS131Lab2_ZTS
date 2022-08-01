3b094d3b2c973fcceaee0a6ed36c06f2d5edf675
Zac-Sells <zsells@bellarmine.edu>
2022-08-01 13:05:31 -0400

    first commit

---
+++ src/Application.java
@@ -0,0 +1,45 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		//create new wizard using empty argument constructor 
+		Wizard W1 = new Wizard();
+		System.out.println(W1.toString());
+		
+		//create new wizard using preferred constructor
+		Wizard W2 = new Wizard("Merlin", 100, 0, false);
+		System.out.println(W2.toString());
+		
+		//implement takeDamage method
+		W2.takeDamage(50);
+		System.out.println(W2.toString());
+		
+		//implement setKey method
+		W2.setKey(15);
+		System.out.println(W2.toString());
+		
+		//implement a second setKey method to demonstrate that it will not set a key a second time
+		W2.setKey(25);
+		System.out.println(W2.toString());
+		
+		//implement lock method that does not match the key to prove it will not lock unless it matches
+		W2.lock(1);
+		System.out.println(W2.toString());
+		
+		//implement lock method that does match the key to prove it will lock
+		W2.lock(15);
+		System.out.println(W2.toString());
+		
+		//implement unlock method that does not match the key to prove it will not unlock unless it matches
+		W2.unlock(1);
+		System.out.println(W2.toString());
+		
+		//implement unlock method that does match the key to prove it will unlock
+		W2.unlock(15);
+		System.out.println(W2.toString());
+		
+		//implement isLocked method 
+		W2.isLocked();
+		System.out.println(W2.isLocked());
+	}
+}
---
+++ src/Wizard.java
@@ -0,0 +1,129 @@
+
+/**
+ * This class represents a wizard for
+ * a D&D type game.
+ * @author rkelley
+ * @version 1.0
+ * Lab 2
+ * CS131ON
+ *
+ */
+public class Wizard implements Lockable{
+	
+	private String name;
+	private int health;
+	private int key;
+	boolean locked;
+	
+	/**
+	 * End constructor to put object into consistent state
+	 */
+	public Wizard() {
+	super();
+	this.name="";
+	this.key = 0;
+	this.health = 100;
+	this.locked = false;
+	}//end constructor
+	
+	/**
+	 * Constructor to create object with common
+	 * usage. This is the preferred constructor.
+	 * @param name
+	 */
+	public Wizard(String name, int health, int key, boolean locked) {
+	super();
+	this.setName(name);
+	this.setKey(key);
+	this.setHealth(health);
+	this.locked = locked;
+	
+
+	}//end constructor
+	
+	/**
+	 * This method processes the 
+	 * damage to the object when 
+	 * it is hit by other objects
+	 * (e.g. weapons, lightening, etc.)
+	 * @param power
+	 */
+	public void takeDamage(int power) {
+			//if (isLocked() != locked)
+			health = health - power;
+	}//end takeDamage
+	
+	/**
+	 * Getter for name
+	 * @return
+	 */
+	public String getName() {
+	       return name;
+	}//end getName
+	
+	/**
+	 * Setter for name
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}//end setName
+	
+	/**
+	 * Getter for health
+	 * @return
+	 */
+	public int getHealth() {
+		return health;
+	}//end getHealth
+	
+	/**
+	 * Setter for health
+	 * @param health
+	 */
+	public void setHealth(int health) {
+		this.health=health;
+	}//end setHealth
+	
+	/**
+	 * Getter for the key
+	 * @return
+	 */
+	public int getKey() {
+		return key;
+	}//end getKey
+
+	@Override
+	public String toString() {
+		return "Wizard [name=" + name + ", health=" + health + ", key=" + key + ", locked=" + locked + "]";
+	}//end toString
+
+	@Override
+	public void setKey(int key) {
+		// TODO Auto-generated method stub
+		if(key > 0 && this.key == 0 )
+		this.key=key;
+			
+	}
+
+	@Override
+	public void lock(int key) {
+		// TODO Auto-generated method stub
+		if(this.key == key)
+			this.locked=true;
+	}
+
+	@Override
+	public void unlock(int key) {
+		// TODO Auto-generated method stub
+		if(this.key == key)
+			this.locked=false;
+	}
+
+	@Override
+	public boolean isLocked() {
+		// TODO Auto-generated method stub
+		return this.locked;
+	}
+	
+}//end class
